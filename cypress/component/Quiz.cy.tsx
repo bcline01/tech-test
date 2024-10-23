2b99c57f13e3edc8559b8cba653496df4b271d35
bcline01 <brookecline.bc@gmail.com>
2024-10-22 23:05:49 -0400

    updating components

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -1,11 +1,23 @@
 // import { it } from 'vitest';
-import Quiz from '../../client/src/components/Quiz'
+// import { mount } from 'cypress/react18';
+import Quiz from '../../client/src/components/Quiz';
 
 describe('Quiz', () => {
     beforeEach(() => {
-        cy.mount(<Quiz />)
+        cy.intercept({
+            method: 'GET',
+            url: '/api/questions/random'
+          },
+          {
+            fixture: 'questions.json',
+            statusCode: 200
+          }
+          ).as('getRandomQuestion')
+        });
+        
       });
     it('renders a quiz', () => {
+        cy.mount(<Quiz />);
         cy.get('.btn').should('exist');
         cy.get('.btn').should('be.visible');
     });
@@ -13,19 +25,27 @@ describe('Quiz', () => {
     // click the start quiz button
     // check that the first question is displayed
     it('clicks the start quiz button', () => {
+        cy.mount(<Quiz />);
         cy.contains('Start Quiz').should('be.visible');
         cy.contains('Start Quiz').click();
-        cy.get('.card h2').should('exist');
+        cy.get('.card').should('be.visible');
     });
+
 //   answer the first question
     it('answers the first question', () => {
+        cy.mount(<Quiz />);
+        cy.wait('@getRandomQuestion').then((intercept) => {
+
         cy.contains('Start Quiz').click();
         cy.get('.card h2').should('exist');
         cy.get('.btn-primary').first().click();
         cy.get('.card h2').should('exist');
+
+    });
     });
     // check is quiz is completed
     it('should display Quiz Completed after answering all questions', () => {
+        cy.mount(<Quiz />);
         cy.contains('Start Quiz').click();
         cy.get('.btn-primary').each((button) => {
         cy.wrap(button).click();
@@ -35,6 +55,7 @@ describe('Quiz', () => {
 
 // should restart the quiz after completion
 it('should restart the quiz after completion', () => {
+    cy.mount(<Quiz />);
     cy.contains('Start Quiz').click();
     cy.get('.btn-primary').each((button) => {
     cy.wrap(button).click();
@@ -43,5 +64,5 @@ it('should restart the quiz after completion', () => {
     cy.get('.btn-primary').should('exist');
 
 });
-});
+
 
